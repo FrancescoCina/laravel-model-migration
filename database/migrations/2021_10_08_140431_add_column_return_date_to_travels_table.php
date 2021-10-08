7ef96b943940e1d86ecb74988d86561fe56b0cf2
FrancescoCina <francesco.cina04@gmail.com>
2021-10-08 16:13:45 +0200

    Aggiunta colonna per la data di ritorno

--- database/migrations/2021_10_08_140431_add_column_return_date_to_travels_table.php
+++ database/migrations/2021_10_08_140431_add_column_return_date_to_travels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelsTable extends Migration
+class AddColumnReturnDateToTravelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateTravelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('travels', function (Blueprint $table) {
-            $table->id();
-            $table->string('destination', 100);
-            $table->dateTime('departure_date');
-            $table->float('price', 10, 2)->unsigned();
-            $table->timestamps();
+        Schema::table('travels', function (Blueprint $table) {
+            $table->datetime('return_date')->after('departure_date');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateTravelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('travels');
+        Schema::table('travels', function (Blueprint $table) {
+            $table->dropColumn('return_date');
+        });
     }
 }
